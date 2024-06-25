48152b1d4c50dd3734d0d2ae530c5b3db2b742d3
Ibrahim8879 <ibrahimqayyum7891234@gmail.com>
2024-06-25 16:11:51 +0500

    Add Sqlmodel

--- app/app/main.py
+++ app/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 app = FastAPI()
 
@@ -6,3 +7,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Server is up and running"}
+
+
+
+@app.get("/new")
+async def root():
+    return {"message": "New page is up and running."}
+
+
+
+@app.post("/add")
+async def root2(data: str, data2: str):
+    return {"message": 'Data received: ' + data + ' and ' + data2}
